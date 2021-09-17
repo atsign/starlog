b13b4bd87c135a5460bf93d4ed3d558b407fa5a8
Andrew Iafrate <a.a.iafrate@gmail.com>
2021-09-16 21:24:47 -0400

    Add authentication pages and workflow

---
+++ src/routes/login.svelte
@@ -0,0 +1,9 @@
+<h1>Login</h1>
+<ul>
+    <li><a rel="external" href="/.auth/login/github?post_login_redirect_uri={baseUrl}/observations">Login with GitHub</a></li>
+    <li><a rel="external" href="/.auth/login/twitter?post_login_redirect_uri={baseUrl}/observations">Login with Twitter</a></li>
+</ul>
+
+<script>
+    let baseUrl = import.meta.env.VITE_PUBLIC_BASE_PATH;
+</script>
